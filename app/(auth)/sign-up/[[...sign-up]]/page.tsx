c64e4fc48e4bf7109e94f6cb6a9487944b9e0e19
employeemanagement13-glitch <employeemanagement13@gmail.com>
2026-03-30 23:12:24 +0500

    Update page.tsx

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -93,7 +93,7 @@ export default function Page() {
                     routing="path"
                     path="/sign-up"
                     signInUrl="/sign-in"
-                    redirectUrl="/admin/dashboard"
+                    redirectUrl="/admin/"
                     fallbackRedirectUrl="/"
                 />
                 
